db9daac38833fb4991c1485fa5d87ca692a3786e
RAGUNAROCKS <jrbotani11@gmail.com>
2023-03-09 08:09:43 -0300

    first commit

---
+++ PPR.rb
@@ -0,0 +1,8 @@
+print "Digite seu nome: "
+firstname = gets.chomp
+print "Digite seu sobrenome: "
+lastname = gets.chomp
+print "Digite sua idade: "
+age = gets.chomp.to_i
+
+print "#{firstname} #{lastname}, #{age} anos"
